d2dd5bcc7e2d188437138529e873aff0cd55f9d8
Sean Huvaya <sean.ktxdev@gmail.com>
2026-02-24 17:29:28 -0500

    Remove all files and initialize project

--- scripts/create_kafka_topics.sh
+++
@@ -1,4 +0,0 @@
-docker exec kafka-broker /opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka-broker:9092 --create --if-not-exists --topic nyc311.raw --partitions 5 --replication-factor 1
-
-echo "Created Kafka topics:"
-docker exec kafka-broker /opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
