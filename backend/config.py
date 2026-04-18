2b7c4a0f65a48f35a57e4884acc9a734b82ef212
Sean Huvaya <sean.ktxdev@gmail.com>
2026-04-17 19:38:04 -0500

    Add debug line

--- backend/config.py
+++ backend/config.py
@@ -3,8 +3,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str = Field(default="postgresql+asyncpg://postgres:postgres@postgres:5432/nyc311",
-                              alias="DATABASE_URL")
+    database_url: str = Field(alias="DATABASE_URL")
 
     class Config:
         env_file = ".env"
